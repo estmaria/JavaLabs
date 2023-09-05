416c3cbbd1dd77fc430b49532d26e312622eac8b
estmaria <114110218+estmaria@users.noreply.github.com>
2023-09-05 13:08:05 -0500

    Add files via upload

---
+++ Programming Assignment 1/ProgrammingAssignment1.java
@@ -0,0 +1,13 @@
+public class ProgrammingAssignment1
+/* Maria */
+{
+    public static void main(String[] args)
+    {
+        System.out.println("#          #         #         # # #      #         #");
+        System.out.println("# #      # #        # #        #    #              # #");
+        System.out.println("#   #   #  #       #   #       #    #     #       #   #");
+        System.out.println("#     #    #      # # # #      # # #      #      # # # #");
+        System.out.println("#          #     #       #     #   #      #     #       #");
+        System.out.println("#          #    #         #    #    #     #    #         #");
+    }
+}
